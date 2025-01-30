e2cdcbc382777351374721b57f7d2fd1b9ec6924
jmynn <poldenv1975@gmail.com>
2025-01-30 15:50:07 +0400

    change output directory

--- docker/add_components.sh
+++ docker/add_components.sh
@@ -1,58 +1,50 @@
 #!/bin/bash
 
-STYLE_CONTAINER=".container {
-    width: var(--container);
-    margin: 0 auto;
-}"
-STYLE_WRAPPER=".wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: inherit;
-    width: 100%;
-}"
-cd ../.. 
-# /scripts
-mkdir -p "components/Container/output" 
-cd "./components/Container/output" 
-# /scripts/components/Container/output
+STYLE_CONTAINER=".container{width:var(--container);margin:0auto;}"
+STYLE_WRAPPER=".wrapper{display:flex;flex-direction:column;align-items:center;height:inherit;width:100%;}"
+
+cd ..
+# /output
+mkdir -p "components/Container" 
+cd "./components/Container" 
+# /output/components/Container
 bash /scripts/template/component_with_children.sh "Container"
 bash /scripts/template/style.sh $STYLE_CONTAINER
 
 cd ~
 # /root
-cd ../scripts/components
-# /scripts/components
-mkdir -p "Wrapper/output" 
-cd "./Wrapper/output" 
-# /scripts/components/Wrapper/output
+cd ../output/components
+# /output/components
+mkdir -p "Wrapper" 
+cd "./Wrapper" 
+# /output/components/Wrapper
 bash /scripts/template/component_with_children.sh "Wrapper"
 bash /scripts/template/style.sh $STYLE_WRAPPER
 
 cd ~
 # /root
-cd ../scripts/components
-# /scripts/components
-mkdir -p "Layout/output" 
-cd "./Layout/output" 
-# /scripts/components/Layout/output
+cd ../output/components
+# /output/components
+mkdir -p "Layout" 
+cd "./Layout" 
+# /output/components/Layout
 bash /scripts/template/layout_root.sh
 
 cd ~
 # /root
-cd ../scripts/components
-# /scripts/components
-mkdir -p "Main/output" 
-cd "./Main/output" 
-# /scripts/components/Main/output
+cd ../output/components
+# /output/components
+mkdir -p "Main" 
+cd "./Main" 
+# /output/components/Main
 bash /scripts/template/component.sh "Main"
 bash /scripts/template/style.sh 
 
 cd ~
 # /root
-cd ../scripts/components
-# /scripts/components
-mkdir -p "Page/output" 
-cd "./Page/output" 
-# /scripts/components/Page/output
+cd ../output/components
+# /output/components
+mkdir -p "Page" 
+cd "./Page" 
+# /output/components/Page/output
 bash /scripts/template/page_root.sh 
